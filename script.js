712afc3d24f85dd314835342ac6037bfcaf5b58f
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-07-26 15:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function saveData(){
+  localStorage.setItem('Stored',JSON.stringify(Stored));
+}
+
+let Stored=[];
+
+let btn=document.getElementById('form');
+btn.addEventListener('submit',()=>{
+ let user=document.getElementById('username').value;
+  let password=document.getElementById('password').value;
+  let checked=document.getElementById('checkbox').checked;
+  let obj={
+    username:user,
+    Password:password
+  }
+  if(checked){
+   Stored.push(obj)
+   saveData();
+  }
+})
